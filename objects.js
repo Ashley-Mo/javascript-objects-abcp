df32161232e7a6c3c2eab17adbf623aa9c98de42
Aswathylal Mohanlal <lal.aswathy@gmail.com>
2018-01-23 22:44:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,5 +4,9 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName]='songTitle';
 }
 function removeFromPlaylist(playlist, artistName){
+<<<<<<< HEAD
  delete playlist[artistName];
+=======
+ return delete playlist.artistName;
+>>>>>>> e0cd229e63cec5b97782f2ca5d76ebb1a16fdeba
 }
